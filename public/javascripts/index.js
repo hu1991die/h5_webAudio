5dbfa5990e1099aa5189441dc9883840df469363
feizi <347431996@qq.com>
2015-05-25 22:59:38 +0800

    使用h5的canvas画布功能将音乐数据制作成柱状图和圆形图动态展示，形成可视化的效果。。柱状图、圆点图

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -32,8 +32,12 @@ var count = 0;
 
 //定义一个分析音频资源的对象
 var analyserNode = ac.createAnalyser();
+
+//全局变量，数据的长度
+var size = 128;
+
 //设置analyserNode节点的fft属性的大小，即每次分析的资源的大小
-analyserNode.fftSize = 512;
+analyserNode.fftSize = size * 2;
 analyserNode.connect(gainNode);
 
 
@@ -108,7 +112,11 @@ function visualizer(){
 						
 	function v(){
 		analyserNode.getByteFrequencyData(arr);
-		console.log(arr);
+		//console.log(arr);
+		
+		//绘制图形（柱状图，圆点）
+		drawGraph(arr);
+		
 		requestAnimationFrame(v);
 	}
 	
@@ -117,6 +125,159 @@ function visualizer(){
 
 //调用音频分析函数
 visualizer();
+
+
+//将音频资源进行可视化制作，取得盒子div(放置柱状图)
+var box = $("#box")[0];
+var height,width;
+
+//定义一个canvas画布元素
+var canvas = document.createElement('canvas');
+
+//获取ctx上下文对象,(2D图形)
+var ctx = canvas.getContext("2d");
+
+//将画布canvas添加到div展示区中
+box.appendChild(canvas);
+
+//定义一个线性渐变,纵向
+var line;
+
+//定义一个圆形渐变
+var g;
+
+/**
+ * 产生一个m-n之间的一个整数
+ * @param {Object} m
+ * @param {Object} n
+ */
+function random(m,n){
+	return Math.round(Math.random()*(n - m) + m);
+}
+
+//点的集合数组
+var Dots = [];
+
+function getDots(){
+	//每次调用之间清空Dots数组中的数组
+	Dots = [];
+	for(var i = 0; i < size; i++){
+		var x = random(0, width);
+		var y = random(0,height);
+		var color = "rgb(" + random(0,255) + "," + random(0,255) + "," + random(0,255) + ")";
+		
+		Dots.push({
+			x: x,
+			y: y,
+			color: color
+		});
+	}
+}
+
+/**
+ * 创建一个函数，在窗口大小改变的时候动态地获取窗口的高度和宽度
+ */
+function resize(){
+	//获取当前窗口的高度和宽度
+	height = box.clientHeight;
+	width = box.clientWidth;
+	
+	//分别给canvas的高度和宽度赋值
+	canvas.height = height;
+	canvas.width = width;
+	
+	//创建线性渐变,纵向
+	line = ctx.createLinearGradient(0, 0, 0, height);
+	
+	//为线性渐变添加渐变色
+	line.addColorStop(0,"red");
+	line.addColorStop(0.5,"yellow");
+	line.addColorStop(1,"green");
+	
+	//每次改变窗口大小的时候，获取Dot数组
+	getDots();
+	
+}
+
+//先初始化加载一次
+resize();
+
+//getDots();
+
+//调用resize()函数，当窗口改变的时候调用
+window.onresize = resize;
+
+/**
+ * 绘制图形（柱状图，圆点）
+ * @param {Object} arr
+ */
+function drawGraph(arr){
+	//每次在绘制之前需要将上一次的数据给清除掉，不然会有一个叠加的过程
+	ctx.clearRect(0, 0, width, height);
+	
+	//宽度
+	var w = width / size;
+	
+	//应用到ctx的sillStyle上,每次调用之前默认应用上线性渐变
+	ctx.fillStyle = line;
+	
+	//for循环
+	for(var i = 0;i < size; i++){
+		
+		//判断绘制图形的类型（柱状还是点状）
+		if(drawGraph.type == "column"){
+			
+			//高度是动态变化的
+			var h = (arr[i] / 256) * height;
+		
+			//填充canvas
+			ctx.fillRect(w * i, height - h, w * 0.6, h);
+		}else if(drawGraph.type == "dot"){
+			//重新绘制(起始位置归0)
+			ctx.beginPath();
+			
+			var o = Dots[i];
+			var r = (arr[i] / 256) * 50;
+			
+			//绘制圆形
+			ctx.arc(o.x, o.y, r, 0, Math.PI*2, true);
+			
+			//创建一个渐变
+			g = ctx.createRadialGradient(o.x, o.y, 0, o.x, o.y, r);
+			g.addColorStop(0,"#fff");
+			g.addColorStop(1,o.color);
+			
+			ctx.fillStyle = g;
+			ctx.fill();
+			
+			//白色
+			/*ctx.strokeStyle = "#fff";
+			ctx.stroke();*/
+		}
+		
+	}
+}
+
+//为drawGraph函数绑定一个属性（是画柱状图还是圆点）,默认是画柱状图
+drawGraph.type = "column";
+
+//获取可视化显示的类型
+var types = $("#type li");
+
+/**
+ * 鼠标点击的时候，改变显示效果
+ */
+for(var i = 0; i < types.length; i++){
+	types[i].onclick = function(){
+		for(var j = 0; j < types.length; j++){
+			types[j].className = "";
+		}
+		this.className = "selected";
+		
+		//获取类型
+		drawGraph.type = this.getAttribute("data-type");
+	}
+}
 
 /**
  * 改变音量大小
